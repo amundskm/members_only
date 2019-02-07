f58b900ef0c6a73c2a30feaa8982c7a3bdfc87cb
amundskm <amundskm@gmail.com>
2019-02-07 14:26:22 -0600

    add testing to app

--- test/controllers/account_activations_controller_test.rb
+++ test/controllers/account_activations_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class ActivationControllerTest < ActionDispatch::IntegrationTest
+class AccountActivationsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
