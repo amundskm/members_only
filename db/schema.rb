8e09ecd30429b61e0064d7b7a3c0ce4965f151f4
amundskm <amundskm@gmail.com>
2019-02-07 14:11:03 -0600

    add name to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_07_195324) do
+ActiveRecord::Schema.define(version: 2019_02_07_200949) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_02_07_195324) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "name"
   end
 
 end
