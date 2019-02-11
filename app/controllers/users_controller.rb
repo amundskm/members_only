04d4a18c1fa5a909f2e4b365e02a15861cca74d8
amundskm <amundskm@gmail.com>
2019-02-11 11:11:43 -0600

    only allow logged in to see user pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,15 @@ class UsersController < ApplicationController
   before_action :admin_user, only: [:edit, :destroy]
 
   def index
-    @users = User.where(activated: true).paginate(page: params[:page])
+      @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
-    @user = User.find(params[:id])
-    @posts = @user.posts.paginate(page: params[:page])
-    redirect_to root_url and return unless @user.activated?
+    unless logged_in_user
+      @user = User.find(params[:id])
+      @posts = @user.posts.paginate(page: params[:page])
+      redirect_to root_url and return unless @user.activated?
+    end
   end
 
   def new
