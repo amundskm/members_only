a5198c764abe53d048b33396d91339c7ba4d25e6
amundskm <amundskm@gmail.com>
2019-02-11 10:18:38 -0600

    destroy posts when user destroyed

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,8 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   has_secure_password
-  has_many :posts
-  
+  has_many :posts, dependent: :destroy
+
   # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
