c59a0d200c71b1861b32c37a12f04311ace61487
amundskm <amundskm@gmail.com>
2019-02-11 09:31:00 -0600

    validate posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,17 @@ class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
+  has_secure_password
+  has_many :posts
+  
   # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:michael)
+
+    @post = @user.posts.build(content: "We the People of the United States, in Order to
+      form a more perfect Union, establish Justice, insure domestic Tranquility,
+       provide for the common defense, promote the general Welfare, and secure
+       the Blessings of Liberty to ourselves and our Posterity, do ordain and
+         establish this Constitution for the United States of America.")
+  end
+
+  test "should be valid" do
+    assert @post.valid?
+  end
+
+  test "post should be present" do
+    @post.content = "      "
+    assert_not @post.valid?
+  end
+
+  test "content should not be too long" do
+    @post.content = "a" * 501
+    assert_not @post.valid?
+  end
+
 end
